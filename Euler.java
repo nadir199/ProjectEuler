a24434667bc830f5450932c5f1474cca0447ff40
Debbagh Nadir <nadir199@live.fr>
2017-11-04 03:06:24 +0100

    Done problem 4 Largest palindrome product

--- Euler.java
+++ Euler.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
  * Created by Asus on 20/09/2017.
  */
 public class Euler {
-    // Problem1
+    // Problem1 Sum of all multiples of 3 or 5 less than MAX_NUMBER
     public static int sumMult35_Problem1(int MAX_NUMBER){
         int sum=0;
         for(int i=1;i<MAX_NUMBER;i++)
@@ -15,7 +15,7 @@ public class Euler {
         System.out.println(sum);
         return sum;
     }
-
+    //Problem 2 sum of all fibonacci numbers less than MAX_NUMBER
     public static int sumFibo_Problem2(int MAX_NUMBER){
         int sum=2;
         int prec1=1;
@@ -31,6 +31,8 @@ public class Euler {
         System.out.println(sum);
         return sum;
     }
+
+    //Problem 3 Find the largest prime factor of a number
     public static boolean isPrime(BigInteger number){
         for( BigInteger i= new BigInteger("2");i.compareTo(number.divide(new BigInteger("2")))<0;i=i.add(new BigInteger("1"))){
             if(number.mod(i).equals(BigInteger.ZERO))
@@ -51,4 +53,43 @@ public class Euler {
             return number;
         return largestPrimeFactor_Problem3(number.divide(smallestPrimeFactor(number)));
     }
+
+    //Problem 4 Largest palindrome product of two 2-digit numbers
+    public static BigInteger invertNumber(BigInteger number){
+        BigInteger intVerted=BigInteger.ZERO;
+        while(!number.equals(BigInteger.ZERO)){
+            intVerted= intVerted.multiply(BigInteger.TEN).add(number.mod(BigInteger.TEN));
+            number= number.divide(BigInteger.TEN);
+        }
+        return intVerted;
+    }
+    public static boolean isPalindromic(BigInteger number){
+        return number.equals(invertNumber(number));
+    }
+    public static int getSmallestNDigitsNumber(int n){
+        int smallest=1;
+        for(int i=1;i<n;i++){
+            smallest*=10;
+        }
+        return smallest;
+    }
+    public static int getLargestNDigitsNumber(int n){
+        return getSmallestNDigitsNumber(n)*10 -1;
+    }
+    public static BigInteger getLargestPalindromeProdOf2nDigNumbers_Problem4(int digits){
+        BigInteger smallest=new BigInteger(getSmallestNDigitsNumber(digits)+"");
+        BigInteger biggest = new BigInteger(getLargestNDigitsNumber(digits)+"");
+        BigInteger maxPalindrome=BigInteger.ZERO;
+        for(BigInteger i=new BigInteger(smallest.toString());i.compareTo(biggest)<=0;i=i.add(BigInteger.ONE)){
+            for(BigInteger j=new BigInteger(i.toString());j.compareTo(biggest)<=0;j=j.add(BigInteger.ONE)){
+                BigInteger mult = i.multiply(j);
+                if(mult.compareTo(maxPalindrome)>0 && isPalindromic(mult)) {
+                    maxPalindrome = mult;
+                    System.out.println(maxPalindrome+" = "+ i + " * " + j);
+                }
+            }
+        }
+        return maxPalindrome;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -5,8 +5,11 @@ import java.math.BigInteger;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println(Euler.largestPrimeFactor_Problem3(new BigInteger("600851475143")));
+        //System.out.println(Euler.largestPrimeFactor_Problem3(new BigInteger("600851475143")));
         ///System.out.println(Euler.largestPrimeFactor_Problem3(new BigInteger("600851475143")));
+        System.out.println("Start");
+        System.out.println(Euler.getLargestPalindromeProdOf2nDigNumbers_Problem4(3));
+        System.out.println("ENd");
     }
 
 
